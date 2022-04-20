6a0c791d6f4d38963c3cdf3d23ae356cec3f5005
rusta <rustam_ussr@mail.ru>
2022-04-20 17:09:06 +0300

    1. Что такое итератор. [#4951]

---
+++ src/test/java/ru/job4j/it/BackwardArrayItTest.java
@@ -0,0 +1,44 @@
+package ru.job4j.it;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import org.junit.Test;
+
+import java.util.NoSuchElementException;
+
+public class BackwardArrayItTest {
+
+
+    @Test
+    public void whenMultiCallhasNextThenTrue() {
+        BackwardArrayIt it = new BackwardArrayIt(
+                new int[]{11, 22, 33}
+        );
+        assertThat(it.hasNext(), is(true));
+        it.next();
+        assertThat(it.hasNext(), is(true));
+        it.next();
+        assertThat(it.hasNext(), is(true));
+        it.next();
+        assertThat(it.hasNext(), is(true));
+    }
+
+    @Test
+    public void whenReadSequence() {
+        BackwardArrayIt it = new BackwardArrayIt(
+                new int[]{10, 20, 30}
+        );
+        assertThat(it.next(), is(30));
+        assertThat(it.next(), is(20));
+        assertThat(it.next(), is(10));
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void whenNextFromEmpty() {
+        BackwardArrayIt it = new BackwardArrayIt(
+                new int[]{}
+        );
+        it.next();
+    }
+}
