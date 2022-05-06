bed03ab497ba54acf795082143c3fec58ac39055
Qafas <rustam_ussr@mail.ru>
2022-05-06 13:10:33 +0400

    1. Динамический список на массиве. [#158 #297532]

--- src/main/java/ru/job4j/collection/SimpleArrayList.java
+++ src/main/java/ru/job4j/collection/SimpleArrayList.java
@@ -19,35 +19,30 @@ public class SimpleArrayList<T> implements List<T> {
         if (size < container.length - 1) {
             container[size] = value;
         } else {
-            container = Arrays.copyOf(container, container.length * 2);
+            doubleContainerLength();
             container[size] = value;
         }
         size++;
         modCount++;
     }
 
+    private void doubleContainerLength() {
+        if (container.length == 0) {
+            container = Arrays.copyOf(container, 2);
+        }
+        container = Arrays.copyOf(container, container.length * 2);
+    }
+
     @Override
     public T set(int index, T newValue) {
-        Objects.checkIndex(index, size);
-        T oldValue = container[index];
-        T[] newArray = (T[]) new Object[container.length + 1];
-        int j = 0;
-        for (int i = 0; i < newArray.length - 1; i++) {
-            if (i == index) {
-                newArray[i] = newValue;
-            } else {
-                newArray[i] = container[j];
-                j++;
-            }
-        }
-        container = Arrays.copyOf(newArray, newArray.length);
+        T oldValue = get(index);
+        container[index] = newValue;
         return oldValue;
     }
 
     @Override
     public T remove(int index) {
-        Objects.checkIndex(index, size);
-        T removedElement = container[index];
+        T removedElement = get(index);
         System.arraycopy(container, index + 1, container, index, container.length - index - 1);
         container[container.length - 1] = null;
         size--;
@@ -75,6 +70,9 @@ public class SimpleArrayList<T> implements List<T> {
 
             @Override
             public boolean hasNext() {
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
                 return point < size;
             }
 
@@ -83,9 +81,7 @@ public class SimpleArrayList<T> implements List<T> {
                 if (!hasNext()) {
                     throw new NoSuchElementException();
                 }
-                if (expectedModCount != modCount) {
-                    throw new ConcurrentModificationException();
-                }
+
                 return container[point++];
             }
         };
