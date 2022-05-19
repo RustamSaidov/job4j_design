54756bf6b3f763086a0f652a1e7427ad7cf2d860
Qafas <rustam_ussr@mail.ru>
2022-05-19 21:24:30 +0400

    3. Переопределить только hashCode [#1003]

--- src/main/java/ru/job4j/hashmap/User.java
+++ src/main/java/ru/job4j/hashmap/User.java
@@ -14,8 +14,16 @@ public class User {
         this.birthday = birthday;
     }
 
-    @Override
+/*    @Override
     public int hashCode() {
         return Objects.hash(name, children, birthday);
+    }*/
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return children == user.children && name.equals(user.name) && birthday.equals(user.birthday);
     }
 }
