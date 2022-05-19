2bc95057b557d096639d3b76427ae408c2c66d71
rusta <rustam_ussr@mail.ru>
2022-05-19 15:56:13 +0300

    2. Без переопределения equals и hashCode [#1005]

---
+++ src/main/java/ru/job4j/hashmap/LearningHashMap.java
@@ -0,0 +1,17 @@
+package ru.job4j.hashmap;
+
+import java.util.Calendar;
+import java.util.HashMap;
+
+public class LearningHashMap {
+    public static void main(String[] args) {
+        User user1 = new User("John", 1, Calendar.getInstance());
+        User user2 = new User("John", 1, Calendar.getInstance());
+        System.out.println(user1);
+        System.out.println(user1);
+        HashMap<User, Object> hashMap = new HashMap<>();
+        hashMap.put(user1, new Object());
+        hashMap.put(user2, new Object());
+        System.out.println(hashMap);
+    }
+}
--- src/main/java/ru/job4j/hashmap/User.java
+++ src/main/java/ru/job4j/hashmap/User.java
@@ -1,4 +1,4 @@
-package ru.job4j;
+package ru.job4j.hashmap;
 
 import java.util.Calendar;
 
