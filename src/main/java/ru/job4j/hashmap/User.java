f941b20805b08f1de7cd48b095fa39ee677af9eb
Qafas <rustam_ussr@mail.ru>
2022-05-19 21:35:31 +0400

    4. Переопределить только equals [#1004]

--- src/main/java/ru/job4j/hashmap/User.java
+++ src/main/java/ru/job4j/hashmap/User.java
@@ -14,11 +14,6 @@ public class User {
         this.birthday = birthday;
     }
 
-/*  @Override
-    public int hashCode() {
-        return Objects.hash(name, children, birthday);
-    }*/
-
     @Override
     public boolean equals(Object o) {
         if (this == o) {
