731ec1fdc9080dabf3103fa9d28ac8b37b1f8df9
rusta <rustam_ussr@mail.ru>
2022-05-24 13:05:58 +0300

    8. Реализовать собственную структуру данных - HashMap [#1008]

--- src/main/java/ru/job4j/map/SimpleMap.java
+++ src/main/java/ru/job4j/map/SimpleMap.java
@@ -1,54 +1,99 @@
 package ru.job4j.map;
 
+import java.util.ConcurrentModificationException;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Objects;
 
 public class SimpleMap<K, V> implements Map<K, V> {
 
     private static final float LOAD_FACTOR = 0.75f;
-
     private int capacity = 8;
-
     private int count = 0;
-
     private int modCount = 0;
-
     private MapEntry<K, V>[] table = new MapEntry[capacity];
 
     @Override
     public boolean put(K key, V value) {
-        table[key]=value;
-        return false;
+        expand();
+        boolean result = false;
+        int index = indexFor(hash(key.hashCode()));
+        if (table[index] == null) {
+            table[index] = new MapEntry<>(key, value);
+            count++;
+            modCount++;
+            result = true;
+        }
+        return result;
     }
 
     private int hash(int hashCode) {
-        return 0;
+        return (hashCode == 0) ? 0 : (hashCode ^ (hashCode >>> 16));
     }
 
     private int indexFor(int hash) {
-        return 0;
+        return hash & (table.length - 1);
     }
 
     private void expand() {
-
+        if (count / LOAD_FACTOR >= 1) {
+            MapEntry<K, V>[] tempTable = new MapEntry[capacity * 2];
+            for (int i = 0; i < table.length; i++) {
+                tempTable[i] = table[i];
+            }
+            table = tempTable;
+        }
     }
 
     @Override
     public V get(K key) {
-        return ;
+        MapEntry<K, V> result = table[indexFor(hash(key.hashCode()))];
+        if (result == null) {
+            throw new NullPointerException();
+        }
+        return result.value;
     }
 
     @Override
     public boolean remove(K key) {
-        return false;
+        boolean result = false;
+        for (int i = 0; i < table.length; i++) {
+            if (table[indexFor(hash(key.hashCode()))] == table[i]) {
+                table[i] = null;
+                count--;
+                modCount++;
+                result = true;
+            }
+        }
+        return result;
     }
 
     @Override
     public Iterator<K> iterator() {
-        return null;
+        int expectedModCount = modCount;
+        return new Iterator<K>() {
+            private int point = 0;
+
+            @Override
+            public boolean hasNext() {
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return point < capacity;
+            }
+
+            @Override
+            public K next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+
+                return table[point++].key;
+            }
+        };
     }
 
     private static class MapEntry<K, V> {
-
         K key;
         V value;
 
@@ -56,7 +101,5 @@ public class SimpleMap<K, V> implements Map<K, V> {
             this.key = key;
             this.value = value;
         }
-
     }
-
 }
--- src/test/java/ru/job4j/map/SimpleMapTest.java
+++ src/test/java/ru/job4j/map/SimpleMapTest.java
@@ -1,4 +1,68 @@
 package ru.job4j.map;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class SimpleMapTest {
+
+    @Test
+    public void whenAddThenControlAddedValue() {
+        Map<Integer, Integer> map = new SimpleMap<>();
+        assertTrue(map.put(1, 1111));
+        assertEquals((Integer) 1111, map.get(1));
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void whenAddThenRemove() {
+        Map<Integer, Integer> map = new SimpleMap<>();
+        assertTrue(map.put(2, 1111));
+        assertEquals((Integer) 1111, map.get(2));
+        map.remove(2);
+        assertNull(map.get(2));
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void whenAddThenRemoveTwoTimesByTheSameIndex() {
+        Map<Integer, Integer> map = new SimpleMap<>();
+        assertTrue(map.put(1, 1111));
+        assertTrue(map.put(2, 2222));
+        assertEquals((Integer) 2222, map.get(2));
+        map.remove(2);
+        assertNull(map.get(2));
+        map.remove(2);
+        assertNull(map.get(2));
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void whenAddThenRemoveTwoTimes() {
+        Map<Integer, Integer> map = new SimpleMap<>();
+        assertTrue(map.put(1, 1111));
+        assertTrue(map.put(2, 2222));
+        assertEquals((Integer) 1111, map.get(1));
+        assertEquals((Integer) 2222, map.get(2));
+        map.remove(2);
+        assertNull(map.get(2));
+        map.remove(1);
+        assertNull(map.get(1));
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void whenTableExpanding() {
+        Map<Integer, Integer> map = new SimpleMap<>();
+        assertTrue(map.put(12, 9999));
+        assertEquals((Integer) 9999, map.get(3));
+        assertTrue(map.put(1, 1111));
+        assertTrue(map.put(2, 1111));
+        assertTrue(map.put(3, 1111));
+        assertEquals((Integer) 1111, map.get(3));
+        assertTrue(map.put(4, 1111));
+        assertTrue(map.put(5, 1111));
+        assertTrue(map.put(6, 1111));
+        assertTrue(map.put(7, 1111));
+        assertTrue(map.put(8, 1111));
+        assertTrue(map.put(9, 1111));
+        assertTrue(map.put(10, 1111));
+        assertTrue(map.put(11, 1111));
+        assertTrue(map.put(12, 9999));
+        assertEquals((Integer) 9999, map.get(12));
+    }
 }
