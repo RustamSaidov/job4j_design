c853590feafa6c991f96215188f67d690ab28f94
rusta <rustam_ussr@mail.ru>
2022-06-08 15:39:20 +0300

    0.1. FileOutputStream. [#252488 #310242]

---
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -0,0 +1,29 @@
+package ru.job4j.io;
+
+import java.io.FileOutputStream;
+
+public class ResultFile {
+    public static void main(String[] args) {
+
+        try (FileOutputStream out = new FileOutputStream("result.txt")) {
+            for (int i = 0; i < getSimple2DArray().length; i++) {
+                for (int j = 0; j < getSimple2DArray().length; j++) {
+                    out.write((getSimple2DArray()[i][j] + " ").getBytes());
+                }
+                out.write(System.lineSeparator().getBytes());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static int[][] getSimple2DArray() {
+        int[][] simple2DArray = new int[4][4];
+        for (int i = 0; i < simple2DArray.length; i++) {
+            for (int j = 0; j < simple2DArray[0].length; j++) {
+                simple2DArray[i][j] = (i + 1) * (j + 1);
+            }
+        }
+        return simple2DArray;
+    }
+}
