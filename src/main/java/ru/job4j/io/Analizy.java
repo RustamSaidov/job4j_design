1c9543eefddc278155376f3f89e43887bf561ef2
rusta <rustam_ussr@mail.ru>
2022-06-10 14:15:31 +0300

    2. Анализ доступности сервера. [#859]

--- src/main/java/ru/job4j/io/Analizy.java
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -1,34 +1,27 @@
 package ru.job4j.io;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Analizy {
     public void unavailable(String source, String target) {
         boolean serverOffFlag = false;
         String intervalBeginning = "";
         String intervalEnd;
-        List<String> list = new ArrayList<>();
         try (BufferedReader in = new BufferedReader(new FileReader(source))) {
-            for (String line = in.readLine(); line != null; line = in.readLine()) {
-                if ((line.contains("400") || line.contains("500")) && !serverOffFlag) {
-                    intervalBeginning = line.substring(4);
-                    serverOffFlag = true;
+            try (PrintWriter out = new PrintWriter(new BufferedOutputStream(new FileOutputStream(target)))) {
+                for (String line = in.readLine(); line != null; line = in.readLine()) {
+                    if ((line.contains("400") || line.contains("500")) && !serverOffFlag) {
+                        intervalBeginning = line.substring(4);
+                        serverOffFlag = true;
+                    }
+                    if (!(line.contains("400") || line.contains("500")) && serverOffFlag) {
+                        intervalEnd = line.substring(4);
+                        serverOffFlag = false;
+                        out.println(intervalBeginning + ";" + intervalEnd + ";");
+                    }
                 }
-                if (!(line.contains("400") || line.contains("500")) && serverOffFlag) {
-                    intervalEnd = line.substring(4);
-                    serverOffFlag = false;
-                    list.add(intervalBeginning + ";" + intervalEnd + ";");
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        try (PrintWriter out = new PrintWriter(new BufferedOutputStream(new FileOutputStream(target)))) {
-            for (int i = 0; i < list.size(); i++) {
-                out.println(list.get(i));
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -38,11 +31,5 @@ public class Analizy {
     public static void main(String[] args) {
         Analizy analizy = new Analizy();
         analizy.unavailable("./data/server_log.txt", "./data/log_analysis_result_file.txt");
-        /*try (PrintWriter out = new PrintWriter(new FileOutputStream("unavailable.csv"))) {
-            out.println("15:01:30;15:02:32");
-            out.println("15:10:30;23:12:32");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }*/
     }
 }
