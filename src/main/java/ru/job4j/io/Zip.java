b238b7145e2502e21ea5e3b721a489eb5467dd3f
rusta <rustam_ussr@mail.ru>
2022-06-17 09:57:13 +0300

    5.1. Именованные аргументы [#295518]

--- src/main/java/ru/job4j/io/Zip.java
+++ src/main/java/ru/job4j/io/Zip.java
@@ -2,7 +2,6 @@ package ru.job4j.io;
 
 import java.io.*;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
@@ -12,43 +11,25 @@ public class Zip {
     static Search searchobj = new Search();
 
     public void packFiles(List<Path> sources, File target) {
-        for (int i = 0; i < sources.size(); i++) {
-            packSingleFile(sources.get(i).toFile(), target);
-        }
-
-    }
-
-    public void packSingleFile(File source, File target) {
         try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
-            zip.putNextEntry(new ZipEntry(source.getPath()));
-            try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(source))) {
-                zip.write(out.readAllBytes());
+            for (int i = 0; i < sources.size(); i++) {
+                zip.putNextEntry(new ZipEntry(sources.get(i).toFile().getPath()));
+                try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(sources.get(i).toFile()))) {
+                    zip.write(out.readAllBytes());
+                }
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
+
     }
 
     public static void main(String[] args) throws IOException {
-        /*searchobj.argumentsValidation(args);*/
         names = ArgsName.of(args);
+        searchobj.argumentsValidation(names.getValues().values().toArray(new String[0]));
         Path start = Path.of(names.getValues().get("d"));
-        List<Path> listpath = searchobj.search(start, p -> p.toFile().getName().endsWith(names.getValues().get("e")));
+        List<Path> listpath = searchobj.search(start, p -> !p.toFile().getName().endsWith(names.getValues().get("e")));
         Zip zip = new Zip();
         zip.packFiles(listpath, new File(names.getValues().get("o")));
-
-
-
-
-
-
-
-
-/*
-        Zip zip = new Zip();
-        zip.packSingleFile(
-                new File("./pom.xml"),
-                new File("./pom.zip")
-        );*/
     }
 }
