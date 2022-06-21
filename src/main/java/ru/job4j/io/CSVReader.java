ce9720ed6021c295ae8a2537d5d1748ea522f407
Qafas <rustam_ussr@mail.ru>
2022-06-21 21:45:57 +0400

    7. Scanner [#504791]

--- src/main/java/ru/job4j/io/CSVReader.java
+++ src/main/java/ru/job4j/io/CSVReader.java
@@ -1,74 +1,53 @@
 package ru.job4j.io;
 
 import java.io.*;
-import java.nio.file.Files;
 import java.util.*;
 
 public class CSVReader {
 
     public static void handle(ArgsName argsName) throws Exception {
-        List<String> dataList = new ArrayList<>();
+        List<String> dataList = convertDataIntoListOfLines(argsName);
         List<String> filteredColumnsNamesList = new ArrayList<>();
-        Set<String> filteredColumnsNamesSet = new HashSet<>();
-
-        int lineCount = 0;
-        int columnsCount = 0;
-        int filteredColumnCount = 0;
-        convertDataIntoListOfLines(argsName, dataList);
-        lineCount = dataList.size();
-        columnsCount = getColumnsCount(dataList, columnsCount, ";");
+        int lineCount = dataList.size();
+        int columnsCount = getColumnsCount(dataList, ";");
         String[][] dataArray = new String[lineCount][columnsCount];
-
         convertingListOfLinesIntoArray(dataList, lineCount, columnsCount, dataArray, ";");
-
         filteredColumnsNamesList.add(argsName.getValues().get("filter"));
-        filteredColumnCount = getColumnsCount(filteredColumnsNamesList, filteredColumnCount, ",");
+        int filteredColumnCount = getColumnsCount(filteredColumnsNamesList, ",");
         String[][] filteredColumnsNamesArray = new String[1][filteredColumnCount];
         convertingListOfLinesIntoArray(filteredColumnsNamesList, 1, filteredColumnCount, filteredColumnsNamesArray, ",");
-
-
-        fillingSetByFilteredColumnsNames(filteredColumnsNamesSet, filteredColumnsNamesArray);
-        System.out.println("set: " + filteredColumnsNamesSet);
-
-        String[][] filteredDataArray = new String[lineCount][filteredColumnsNamesSet.size()];
-
-        filterDataArray(filteredColumnsNamesSet, dataArray, filteredDataArray);
-
-
-        System.out.println(Arrays.deepToString(filteredDataArray));
-        System.out.println("----------------");
-        System.out.println(Arrays.deepToString(filteredColumnsNamesArray));
-        System.out.println("----------------");
-        System.out.println(Arrays.deepToString(dataArray));
-        System.out.println("----------------");
-
+        Set<String> filteredColumnsNamesSet = fillingSetByFilteredColumnsNames(filteredColumnsNamesArray);
+        String[][] filteredDataArray = filterDataArray(filteredColumnsNamesSet, dataArray);
         saveToFilteredList(argsName, filteredDataArray);
-
     }
 
-    private static void filterDataArray(Set<String> filteredColumnsNamesSet, String[][] dataArray, String[][] filteredDataArray) {
+    private static String[][] filterDataArray(Set<String> filteredColumnsNamesSet, String[][] dataArray) {
+        String[][] filteredDataArray = new String[dataArray.length][filteredColumnsNamesSet.size()];
         int k = 0;
         for (int i = 0; i < dataArray.length; i++) {
             for (int j = 0; j < dataArray[0].length; j++) {
                 if (filteredColumnsNamesSet.contains(dataArray[0][j])) {
                     filteredDataArray[i][k] = dataArray[i][j];
-                    System.out.println(filteredDataArray[i][k] + "!!!");
                     k++;
                 }
             }
             k = 0;
         }
+        return filteredDataArray;
     }
 
-    private static void fillingSetByFilteredColumnsNames(Set<String> filteredColumnsNamesSet, String[][] filteredColumnsNamesArray) {
+    private static Set<String> fillingSetByFilteredColumnsNames(String[][] filteredColumnsNamesArray) {
+        Set<String> filteredColumnsNamesSet = new HashSet<>();
         for (int i = 0; i < filteredColumnsNamesArray.length; i++) {
             for (int j = 0; j < filteredColumnsNamesArray[0].length; j++) {
                 filteredColumnsNamesSet.add(filteredColumnsNamesArray[i][j]);
             }
         }
+        return filteredColumnsNamesSet;
     }
 
-    private static void convertDataIntoListOfLines(ArgsName argsName, List<String> dataList) {
+    private static List<String> convertDataIntoListOfLines(ArgsName argsName) {
+        List<String> dataList = new ArrayList<>();
         try (BufferedReader in = new BufferedReader(new FileReader(argsName.getValues().get("path")))) {
             for (String line = in.readLine(); line != null; line = in.readLine()) {
                 dataList.add(line);
@@ -77,13 +56,13 @@ public class CSVReader {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        return dataList;
     }
 
-    private static int getColumnsCount(List<String> dataList, int columnsCount, String delimiter) {
+    private static int getColumnsCount(List<String> dataList, String delimiter) {
+        int columnsCount = 0;
         var scanner = new Scanner(new ByteArrayInputStream(dataList.get(0).getBytes()))
-                .useDelimiter(delimiter
-                        //";"
-                );
+                .useDelimiter(delimiter);
         while (scanner.hasNext()) {
             scanner.next();
             columnsCount++;
@@ -94,9 +73,7 @@ public class CSVReader {
     private static void convertingListOfLinesIntoArray(List<String> list, int lineCount, int columnsCount, String[][] dataArray, String delimiter) {
         for (int i = 0; i < lineCount; i++) {
             var scanner1 = new Scanner(new ByteArrayInputStream(list.get(i).getBytes()))
-                    .useDelimiter(delimiter
-                            //";"
-                    );
+                    .useDelimiter(delimiter);
             for (int j = 0; j < columnsCount; j++) {
                 String str = scanner1.next();
                 dataArray[i][j] = str;
@@ -108,36 +85,16 @@ public class CSVReader {
         try (PrintWriter out = new PrintWriter(new BufferedOutputStream(new FileOutputStream(argsName.getValues().get("out"))))) {
             for (int i = 0; i < filteredDataArray.length; i++) {
                 for (int j = 0; j < filteredDataArray[0].length; j++) {
-                    out.print(filteredDataArray[i][j] + " ");
+                    if (j < filteredDataArray[0].length - 1) {
+                        out.print(filteredDataArray[i][j] + ";");
+                    } else {
+                        out.print(filteredDataArray[i][j]);
+                    }
                 }
                 out.println();
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
-
-    public static void main(String[] args) throws Exception {
-        String data = String.join(
-                System.lineSeparator(),
-                "name;age;last_name;education",
-                "Tom;20;Smith;Bachelor",
-                "Jack;25;Johnson;Undergraduate",
-                "William;30;Brown;Secondary special"
-        );
-        File file = new File("source.csv");
-        File target = new File("target.csv");
-        ArgsName argsName = ArgsName.of(new String[]{
-                "-path=" + file.getAbsolutePath(), "-delimiter=;", "-out=" + target.getAbsolutePath(), "-filter=name,age"
-        });
-        Files.writeString(file.toPath(), data);
-        String expected = String.join(
-                System.lineSeparator(),
-                "name;age",
-                "Tom;20",
-                "Jack;25",
-                "William;30"
-        ).concat(System.lineSeparator());
-        CSVReader.handle(argsName);
     }
 }
--- src/test/java/ru/job4j/io/CSVReaderTest.java
+++ src/test/java/ru/job4j/io/CSVReaderTest.java
@@ -39,4 +39,30 @@ public class CSVReaderTest {
         Assert.assertEquals(expected, Files.readString(target.toPath()));
     }
 
+    @Test
+    public void whenFilterTreeColumns() throws Exception {
+        String data = String.join(
+                System.lineSeparator(),
+                "name;age;last_name;education",
+                "Tom;20;Smith;Bachelor",
+                "Jack;25;Johnson;Undergraduate",
+                "William;30;Brown;Secondary special"
+        );
+        File file = temporaryFolder.newFile("source.csv");
+        File target = temporaryFolder.newFile("target.csv");
+        ArgsName argsName = ArgsName.of(new String[]{
+                "-path=" + file.getAbsolutePath(), "-delimiter=;", "-out=" + target.getAbsolutePath(), "-filter=name,age,education"
+        });
+        Files.writeString(file.toPath(), data);
+        String expected = String.join(
+                System.lineSeparator(),
+                "name;age;education",
+                "Tom;20;Bachelor",
+                "Jack;25;Undergraduate",
+                "William;30;Secondary special"
+        ).concat(System.lineSeparator());
+        CSVReader.handle(argsName);
+        Assert.assertEquals(expected, Files.readString(target.toPath()));
+    }
+
 }
