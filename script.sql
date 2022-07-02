edff2c51cd289a0a915ad552672bb94e13b14d76
Qafas <rustam_ussr@mail.ru>
2022-07-02 22:39:16 +0400

    0. Что такое база данных. [#6860]

---
+++ script.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE public.mecenates
+(
+   id serial primary key,   	
+   name character varying(255), 
+   surname character varying(255), 
+   age integer, 
+   total_donation bigint 
+);
+
+INSERT INTO public.mecenates(
+            name, surname, age, total_donation)
+    VALUES ('John', 'Smith', 50, 50000);
+
+UPDATE public.mecenates
+   set name = 'Иван', surname='Иванов', age=40, total_donation=100000;
+
+DELETE FROM public.mecenates;
