35fc78e77aa9795843760c84eb98a51785d99a81
Qafas <rustam_ussr@mail.ru>
2022-07-03 20:37:53 +0400

    1. Схема прав пользователи и роли. [#1093]

---
+++ sql/create.sql
@@ -0,0 +1,60 @@
+﻿create table role(
+    id serial primary key,
+    role_name varchar(255)
+);
+
+ create table rules(
+     id serial primary key,
+     rule_name varchar(255)
+ );
+
+ create table category(
+     id serial primary key,
+     category_name varchar(255)
+ );
+
+ create table state(
+     id serial primary key,
+     state_name varchar(255)
+ );
+
+create table users(
+    id serial primary key,
+    nickname varchar(255),
+    role_id int references role(id)
+);
+ 
+ create table role_rules(
+     id serial primary key,
+     role_id int references role(id),
+     rules_id int references rules(id)
+ );
+
+create table item(
+    id serial primary key,
+    user_id int references users(id),
+    category_id int references category(id),
+    state_id int references state(id)
+
+);
+
+create table comments(
+    id serial primary key,
+    comment varchar(255),
+    item_id int references item(id)
+);
+
+create table attachs(
+    id serial primary key,
+    attach_file varchar(255),
+    item_id int references item(id)
+);
+
+
+
+
+
+
+
+
+
---
+++ sql/insert.sql
@@ -0,0 +1,93 @@
+INSERT INTO public.role(
+            role_name)
+    VALUES ('admin');
+
+INSERT INTO public.role(
+            role_name)
+    VALUES ('user');
+
+INSERT INTO public.rules(
+            rule_name)
+    VALUES ('full');
+
+INSERT INTO public.rules(
+            rule_name)
+    VALUES ('read');
+
+INSERT INTO public.category(
+            category_name)
+    VALUES ('urgent');
+
+INSERT INTO public.category(
+            category_name)
+    VALUES ('ordinary');
+
+INSERT INTO public.state(
+            state_name)
+    VALUES ('completed');
+
+INSERT INTO public.state(
+            state_name)
+    VALUES ('in progress');
+    
+INSERT INTO public.state(
+            state_name)
+    VALUES ('not started');
+
+INSERT INTO public.users(
+            nickname, role_id)
+    VALUES ('adminBoy', 1);
+
+INSERT INTO public.users(
+            nickname, role_id)
+    VALUES ('userGirl', 2);
+
+INSERT INTO public.role_rules(
+             role_id, rules_id)
+    VALUES ( 1, 1);
+
+INSERT INTO public.role_rules(
+             role_id, rules_id)
+    VALUES ( 2, 2);
+
+INSERT INTO public.item(
+            user_id, category_id, state_id)
+    VALUES (1, 1, 2);
+
+INSERT INTO public.item(
+            user_id, category_id, state_id)
+    VALUES (2, 2, 1);
+
+INSERT INTO public.item(
+            user_id, category_id, state_id)
+    VALUES (2, 2, 3);
+
+INSERT INTO public.comments(
+            comment, item_id)
+    VALUES ('Срочно нахой! Это АДмин!', 1);
+
+
+INSERT INTO public.comments(
+            comment, item_id)
+    VALUES ('Такую же, но с перламутровыми пуговицами', 2);
+
+INSERT INTO public.comments(
+            comment, item_id)
+    VALUES ('Такую же, но без крыльев', 3);
+
+INSERT INTO public.attachs(
+            attach_file, item_id)
+    VALUES ('file.txt', 1);
+
+INSERT INTO public.attachs(
+            attach_file, item_id)
+    VALUES ('userFile.txt', 2);
+
+INSERT INTO public.attachs(
+            attach_file, item_id)
+    VALUES ('anotherUserFile1.txt', 3);
+
+INSERT INTO public.attachs(
+            attach_file, item_id)
+    VALUES ('anotherUserFile2.txt', 3);
+
