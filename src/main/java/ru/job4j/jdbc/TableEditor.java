7ce23dcedc269277786646f41b1ed0a01cfab252
Qafas <rustam_ussr@mail.ru>
2022-07-12 22:02:39 +0400

    0.1. Statement [#379306]

--- src/main/java/ru/job4j/jdbc/TableEditor.java
+++ src/main/java/ru/job4j/jdbc/TableEditor.java
@@ -9,7 +9,7 @@ import java.util.StringJoiner;
 
 public class TableEditor implements AutoCloseable {
 
-    private static Connection connection;
+    private Connection connection;
 
     private Properties properties;
 
@@ -19,93 +19,77 @@ public class TableEditor implements AutoCloseable {
     }
 
     private void initConnection() throws ClassNotFoundException, SQLException {
-
-        Config config = new Config("src\\main\\resources\\TableEditor.properties");
-        config.load();
-        Class.forName(config.getValues().get("hibernate.connection.driver_class"));
-        String url = config.getValues().get("hibernate.connection.url");
-        String login = config.getValues().get("hibernate.connection.username");
-        String password = config.getValues().get("hibernate.connection.password");
+        Class.forName(properties.getProperty("hibernate.connection.driver_class"));
+        String url = properties.getProperty("hibernate.connection.url");
+        String login = properties.getProperty("hibernate.connection.username");
+        String password = properties.getProperty("hibernate.connection.password");
         connection = DriverManager.getConnection(url, login, password);
     }
 
-    private static void createTable(String tableName) {
+    public void executeQuery(String tableName, String sql, Connection connection) {
         try (Statement statement = connection.createStatement()) {
-            String sql = String.format(
-                    "create table if not exists %s(%s, %s);",
-                    tableName,
-                    "id serial primary key",
-                    "name text"
-            );
             statement.execute(sql);
-            System.out.println(getTableScheme(connection, tableName));
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
 
-    private static void addColumn(String tableName, String columnName, String type) {
-        try (Statement statement = connection.createStatement()) {
-            String sql1 = String.format(
-                    "ALTER TABLE %s ADD COLUMN IF NOT EXISTS %s %s;",
-                    tableName,
-                    columnName,
-                    type
-            );
-            statement.execute(sql1);
-            System.out.println(getTableScheme(connection, tableName));
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+    public void createTable(String tableName) throws Exception {
+        String sql = String.format(
+                "create table if not exists %s(%s, %s);",
+                tableName,
+                "id serial primary key",
+                "name text"
+        );
+        executeQuery(tableName, sql, connection);
+        System.out.println(getTableScheme(connection, tableName));
     }
 
-    private static void renameColumn(String tableName, String columnName, String newColumnName) {
-        try (Statement statement = connection.createStatement()) {
-            String sql2 = String.format(
-
-                    "DO $$\n"
-                            + "BEGIN\n"
-                            + "  IF EXISTS(SELECT * FROM information_schema.columns\n"
-                            + "    WHERE table_name='" + tableName + "' and column_name='" + columnName + "')\n"
-                            + "  THEN\n"
-                            + "      ALTER TABLE %s RENAME COLUMN %s TO %s;\n"
-                            + "  END IF;\n"
-                            + "END $$;",
-                    tableName,
-                    columnName,
-                    newColumnName
-            );
-            statement.execute(sql2);
-            System.out.println(getTableScheme(connection, tableName));
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+    public void addColumn(String tableName, String columnName, String type) throws Exception {
+        String sql = String.format(
+                "ALTER TABLE %s ADD COLUMN IF NOT EXISTS %s %s;",
+                tableName,
+                columnName,
+                type
+        );
+        executeQuery(tableName, sql, connection);
+        System.out.println(getTableScheme(connection, tableName));
     }
 
-    private static void dropColumn(String tableName, String columnName)  {
-        try (Statement statement = connection.createStatement()) {
-            String sql4 = String.format(
-                    "ALTER TABLE %s DROP COLUMN IF EXISTS %s;",
-                    tableName,
-                    columnName
-            );
-            statement.execute(sql4);
-            System.out.println(getTableScheme(connection, tableName));
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+    public void renameColumn(String tableName, String columnName, String newColumnName) throws Exception {
+        String sql = String.format(
+                "DO $$\n"
+                        + "BEGIN\n"
+                        + "  IF EXISTS(SELECT * FROM information_schema.columns\n"
+                        + "    WHERE table_name='" + tableName + "' and column_name='" + columnName + "')\n"
+                        + "  THEN\n"
+                        + "      ALTER TABLE %s RENAME COLUMN %s TO %s;\n"
+                        + "  END IF;\n"
+                        + "END $$;",
+                tableName,
+                columnName,
+                newColumnName
+        );
+        executeQuery(tableName, sql, connection);
+        System.out.println(getTableScheme(connection, tableName));
     }
 
-    private static void dropTable(String tableName)  {
-        try (Statement statement = connection.createStatement()) {
-            String sql5 = String.format(
-                    "DROP TABLE IF EXISTS %s",
-                    tableName
-            );
-            statement.execute(sql5);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+    public void dropColumn(String tableName, String columnName) throws Exception {
+        String sql = String.format(
+                "ALTER TABLE %s DROP COLUMN IF EXISTS %s;",
+                tableName,
+                columnName
+        );
+        executeQuery(tableName, sql, connection);
+        System.out.println(getTableScheme(connection, tableName));
+    }
+
+    public void dropTable(String tableName) {
+        String sql = String.format(
+                "DROP TABLE IF EXISTS %s",
+                tableName
+        );
+        executeQuery(tableName, sql, connection);
     }
 
     public static String getTableScheme(Connection connection, String tableName) throws Exception {
@@ -136,19 +120,16 @@ public class TableEditor implements AutoCloseable {
 
     public static void main(String[] args) throws Exception {
         String tableName = "test_table";
-        Properties config = new Properties();
-        TableEditor tableEditor = new TableEditor(config);
-
-
         try (InputStream in = TableEditor.class.getClassLoader().getResourceAsStream("TableEditor.properties")) {
+            Properties config = new Properties();
             config.load(in);
-
-            createTable(tableName);
-            addColumn(tableName, "new_column", "VARCHAR(255)");
-            renameColumn(tableName, "new_column", "renamed_column");
-            dropColumn(tableName, "renamed_column");
-            dropTable(tableName);
-
+            try (TableEditor tableEditor = new TableEditor(config)) {
+                tableEditor.createTable(tableName);
+                tableEditor.addColumn(tableName, "new_column", "VARCHAR(255)");
+                tableEditor.renameColumn(tableName, "new_column", "renamed_column");
+                tableEditor.dropColumn(tableName, "renamed_column");
+                tableEditor.dropTable(tableName);
+            }
         }
     }
 }
