2d377be5c46264628c2fb5d596991e8a86682092
rusta <rustam_ussr@mail.ru>
2022-07-26 11:49:04 +0300

    1. Демонстрация работы GC [#1589]

---
+++ src/main/java/ru/job4j/gc/User.java
@@ -0,0 +1,49 @@
+package ru.job4j.gc;
+
+public class User {
+    public String name;
+    public String email;
+    public int age;
+    public long telephoneNumber;
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        System.out.printf("Removed %d %s%n", age, name);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public long getTelephoneNumber() {
+        return telephoneNumber;
+    }
+
+    public void setTelephoneNumber(int telephoneNumber) {
+        this.telephoneNumber = telephoneNumber;
+    }
+}
