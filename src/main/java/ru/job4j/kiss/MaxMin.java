abdcc819257f6bfe00d4eee4cd970322b9dff1d3
Qafas <rustam_ussr@mail.ru>
2022-08-12 11:34:20 +0300

    1. Реализация кеша на SoftReference [#1592]

--- src/main/java/ru/job4j/kiss/MaxMin.java
+++ src/main/java/ru/job4j/kiss/MaxMin.java
@@ -15,29 +15,31 @@ public class MaxMin {
     }
 
 
-//    public static <T> void main(String[] args) {
-//        List<Integer> list = new ArrayList<>();
-//        list.add(777);
-//        list.add(555);
-//        list.add(222);
-//        list.add(333);
-//        list.add(444);
-//
-//        Comparator<T> comparator = new Comparator<T>() {
-//
-//            @Override
-//            public int compare(T o1, T o2) {
-//                if(o1> o2)
-//                    return 1;
-//                else if(o1< o1)
-//                    return -1;
-//                else
-//                    return 0;
-//            }
-//            }
-//
-//        }
-//        MaxMin maxMin = new MaxMin();
-//        maxMin.
-//    }
+/*    public static <T> void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(777);
+        list.add(555);
+        list.add(222);
+        list.add(333);
+        list.add(444);
+
+        Comparator<T> comparator = new Comparator<T>() {
+
+            @Override
+            public int compare(T o1, T o2) {
+                if(o1> o2)
+                    return 1;
+                else if(o1< o1)
+                    return -1;
+                else
+                    return 0;
+            }
+            }
+
+        }
+        MaxMin maxMin = new MaxMin();
+        maxMin.
+    }
+
+ */
 }
