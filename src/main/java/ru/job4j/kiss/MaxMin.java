dab0ec85fb79745a86d199b483e5fb39a25ec9f0
Qafas <rustam_ussr@mail.ru>
2022-08-13 11:04:25 +0300

    1. Принципы Kiss, Dry и Yagni [#238813]

--- src/main/java/ru/job4j/kiss/MaxMin.java
+++ src/main/java/ru/job4j/kiss/MaxMin.java
@@ -6,18 +6,17 @@ import java.util.List;
 import java.util.function.Predicate;
 
 public class MaxMin<T> {
-    public String max(List<String> value, Comparator<String> comparator) {
+    public T max(List<T> value, Comparator<T> comparator) {
         Predicate<Integer> predicate = x -> x > 0;
         return returnValueByPredicate(value, comparator, predicate);
     }
 
-    public String min(List<String> value, Comparator<String> comparator) {
+    public T min(List<T> value, Comparator<T> comparator) {
         Predicate<Integer> predicate = x -> x < 0;
         return returnValueByPredicate(value, comparator, predicate);
     }
 
-
-    public static <T> void main(String[] args) {
+    public static void main(String[] args) {
         List<String> list = new ArrayList<>();
         list.add("BBB");
         list.add("DDD");
@@ -28,44 +27,51 @@ public class MaxMin<T> {
         list.add("EEE");
         list.add("CCC");
 
-        Comparator comparator = new MyComparator();
+        Comparator comparator = new StringComparator();
 
         MaxMin maxMin = new MaxMin();
         System.out.println("max" + maxMin.max(list, comparator));
         System.out.println("min" + maxMin.min(list, comparator));
-
-
     }
 
-    private String returnValueByPredicate(List<String> list, Comparator<String> comparator, Predicate<Integer> predicate) {
-        String min = null;
-        String max = null;
-        String result;
-        for (int i = 0; i < list.size() - 1; i++) {
-            if (min == null && max == null) {
-                min = list.get(i);
-                max = list.get(i);
-            }
-            if (comparator.compare(list.get(i), list.get(i + 1)) > 0) {
-                if (comparator.compare(list.get(i), max) > 0) {
-                    max = list.get(i);
-                }
-                if (comparator.compare(list.get(i + 1), min) < 0) {
-                    min = list.get(i + 1);
-                }
-            } else if (comparator.compare(list.get(i), list.get(i + 1)) < 0) {
-                if (comparator.compare(list.get(i), min) < 0) {
-                    min = list.get(i);
-                }
-                if (comparator.compare(list.get(i + 1), max) > 0) {
-                    max = list.get(i + 1);
-                }
-            }
-        }
-        if (predicate.test(comparator.compare(min, max))) {
-            result = min;
+    private T returnValueByPredicate(List<T> list, Comparator<T> comparator, Predicate<Integer> predicate) {
+        T min = null;
+        T max = null;
+        T result;
+        if (list.isEmpty()) {
+            result = null;
         } else {
-            result = max;
+            if (list.size() == 1) {
+                min = list.get(0);
+                max = list.get(0);
+            } else {
+                for (int i = 0; i < list.size() - 1; i++) {
+                    if (min == null && max == null) {
+                        min = list.get(i);
+                        max = list.get(i);
+                    }
+                    if (comparator.compare(list.get(i), list.get(i + 1)) > 0) {
+                        if (comparator.compare(list.get(i), max) > 0) {
+                            max = list.get(i);
+                        }
+                        if (comparator.compare(list.get(i + 1), min) < 0) {
+                            min = list.get(i + 1);
+                        }
+                    } else if (comparator.compare(list.get(i), list.get(i + 1)) < 0) {
+                        if (comparator.compare(list.get(i), min) < 0) {
+                            min = list.get(i);
+                        }
+                        if (comparator.compare(list.get(i + 1), max) > 0) {
+                            max = list.get(i + 1);
+                        }
+                    }
+                }
+            }
+            if (predicate.test(comparator.compare(min, max))) {
+                result = min;
+            } else {
+                result = max;
+            }
         }
         return result;
     }
---
+++ src/test/java/ru/job4j/kiss/MaxMinTest.java
@@ -0,0 +1,81 @@
+package ru.job4j.kiss;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class MaxMinTest {
+
+    @Test
+    public void whenMaxXXX() {
+        List<String> list = new ArrayList<>();
+        list.add("BBB");
+        list.add("XXX");
+        list.add("AAA");
+        list.add("DDD");
+        list.add("FFF");
+        list.add("MMM");
+        list.add("EEE");
+        list.add("CCC");
+
+        Comparator<String> comparator = new StringComparator();
+        MaxMin<String> maxMin = new MaxMin<>();
+        assertThat(maxMin.max(list, comparator), is("XXX"));
+    }
+
+    @Test
+    public void whenMinAAA() {
+        List<String> list = new ArrayList<>();
+        list.add("BBB");
+        list.add("XXX");
+        list.add("AAA");
+        list.add("DDD");
+        list.add("FFF");
+        list.add("MMM");
+        list.add("EEE");
+        list.add("CCC");
+
+        Comparator<String> comparator = new StringComparator();
+        MaxMin<String> maxMin = new MaxMin<>();
+        assertThat(maxMin.min(list, comparator), is("AAA"));
+    }
+
+    @Test
+    public void whenMinEqualsMax() {
+        List<String> list = new ArrayList<>();
+        list.add("DDD");
+        list.add("DDD");
+        list.add("DDD");
+
+        Comparator<String> comparator = new StringComparator();
+        MaxMin<String> maxMin = new MaxMin<>();
+        assertThat(maxMin.max(list, comparator), is("DDD"));
+        assertThat(maxMin.min(list, comparator), is("DDD"));
+    }
+
+    @Test
+    public void whenListFromOneElement() {
+        List<String> list = new ArrayList<>();
+        list.add("DDD");
+
+        Comparator<String> comparator = new StringComparator();
+        MaxMin<String> maxMin = new MaxMin<>();
+        assertThat(maxMin.max(list, comparator), is("DDD"));
+        assertThat(maxMin.min(list, comparator), is("DDD"));
+    }
+
+    @Test
+    public void whenListisEmpty() {
+        List<String> list = new ArrayList<>();
+
+        Comparator<String> comparator = new StringComparator();
+        MaxMin<String> maxMin = new MaxMin<String>();
+        assertNull(maxMin.max(list, comparator));
+
+    }
+}
