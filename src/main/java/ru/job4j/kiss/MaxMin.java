884b8e7bdf074ea6e6e2f024eeed4e68486900f2
Qafas <rustam_ussr@mail.ru>
2022-08-16 17:11:44 +0400

    1. Принципы Kiss, Dry и Yagni [#238813 #331763]

--- src/main/java/ru/job4j/kiss/MaxMin.java
+++ src/main/java/ru/job4j/kiss/MaxMin.java
@@ -36,42 +36,10 @@ public class MaxMin<T> {
     }
 
     private T returnValueByPredicate(List<T> list, BiPredicate<T, T> predicate) {
-        T min = null;
-        T max = null;
-        T result;
-        if (list.isEmpty()) {
-            return null;
-        } else {
-            if (list.size() == 1) {
-                min = list.get(0);
-                max = list.get(0);
-            } else {
-                for (int i = 0; i < list.size() - 1; i++) {
-                    if (min == null && max == null) {
-                        min = list.get(i);
-                        max = list.get(i);
-                    }
-                    if (predicate.test(list.get(i), list.get(i + 1))) {
-                        if (predicate.test(list.get(i), max)) {
-                            max = list.get(i);
-                        }
-                        if (!predicate.test(list.get(i + 1), min)) {
-                            min = list.get(i + 1);
-                        }
-                    } else if (!predicate.test(list.get(i), list.get(i + 1))) {
-                        if (!predicate.test(list.get(i), min)) {
-                            min = list.get(i);
-                        }
-                        if (predicate.test(list.get(i + 1), max)) {
-                            max = list.get(i + 1);
-                        }
-                    }
-                }
-            }
-            if (predicate.test(min, max)) {
-                result = min;
-            } else {
-                result = max;
+        T result = list.get(0);
+        for (int i = 0; i < list.size() - 1; i++) {
+            if (predicate.test(list.get(i), result)) {
+                result = list.get(i);
             }
         }
         return result;
