eabb76df569bcda8ddd88f8e1b324ffbcc39e47a
Qafas <rustam_ussr@mail.ru>
2022-08-22 00:08:33 +0400

    3. Шаблонизатор. [#855]

---
+++ src/test/java/ru/job4j/template/GeneratorTest.java
@@ -0,0 +1,66 @@
+/* package ru.job4j.template;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class GeneratorTest {
+
+    @Test
+    public void whenGenerateWell() {
+        Map<String, String> map = new HashMap<>();
+        map.put("name", "Petr Arsentev");
+        map.put("subject", "you");
+        Generator generator = new PhraseGenerator();
+        String phrase = generator.produce("I am a ${name}, Who are ${subject}? ", map);
+        assertThat(phrase).isEqualTo("I am a Petr Arsentev, Who are you? ");
+    }
+
+    @Test
+    public void whenWrongTemplate() {
+        Map<String, String> map = new HashMap<>();
+        map.put("name", "Petr Arsentev");
+        map.put("subject", "you");
+        Generator generator = new PhraseGenerator();
+        assertThrows(IllegalArgumentException.class, () -> {
+            generator.produce("I am a ${name}, Who are ${subject}, ${speciality}? ", map);
+        });
+    }
+
+    @Test
+    public void whenEmptyTemplate() {
+        Map<String, String> map = new HashMap<>();
+        map.put("name", "Petr Arsentev");
+        map.put("subject", "you");
+        Generator generator = new PhraseGenerator();
+        assertThrows(IllegalArgumentException.class, () -> {
+            generator.produce("", map);
+        });
+    }
+
+    @Test
+    public void whenWrongMapKey() {
+        Map<String, String> map = new HashMap<>();
+        map.put("na", "Petr Arsentev");
+        map.put("subject", "you");
+        Generator generator = new PhraseGenerator();
+        assertThrows(IllegalArgumentException.class, () -> {
+            generator.produce("I am a ${name}, Who are ${subject}, ${speciality}? ", map);
+        });
+    }
+
+    @Test
+    public void whenEmptyMap() {
+        Map<String, String> map = new HashMap<>();
+        Generator generator = new PhraseGenerator();
+        assertThrows(IllegalArgumentException.class, () -> {
+            generator.produce("I am a ${name}, Who are ${subject}, ${speciality}? ", map);
+        });
+    }
+}
+
+ */
