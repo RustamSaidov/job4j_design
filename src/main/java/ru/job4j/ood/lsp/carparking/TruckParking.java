854eaf0d80386ebddc838a929db6c2cfe66a0b1c
Qafas <rustam_ussr@mail.ru>
2022-09-20 23:15:24 +0400

    2. Парковка машин [#853 #340280]

--- src/main/java/ru/job4j/ood/lsp/carparking/TruckParking.java
+++ src/main/java/ru/job4j/ood/lsp/carparking/TruckParking.java
@@ -1,6 +1,5 @@
 package ru.job4j.ood.lsp.carparking;
 
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -9,6 +8,7 @@ public class TruckParking implements Parking {
     private Car[] truckParking;
     private int sizeOfAParking;
     private int numberOfCarsInParking;
+    private final Set<Car> setOfCars = new HashSet<>();
 
     public TruckParking(int sizeOfAParking) {
         this.sizeOfAParking = sizeOfAParking;
@@ -18,9 +18,13 @@ public class TruckParking implements Parking {
 
     @Override
     public boolean add(Car car) {
+        if (setOfCars.contains(car)) {
+            return false;
+        }
         if (car.getCarSize() <= PassangerCar.CAR_SIZE || sizeOfAParking == numberOfCarsInParking) {
             return false;
         }
+        setOfCars.add(car);
         truckParking[numberOfCarsInParking++] = car;
         return true;
     }
--- src/test/java/ru/job4j/ood/lsp/carparking/CustomParkingTest.java
+++ src/test/java/ru/job4j/ood/lsp/carparking/CustomParkingTest.java
@@ -7,6 +7,102 @@ import static org.junit.jupiter.api.Assertions.*;
 class CustomParkingTest {
 
     @Test
+    public void whenPassCarGoesToPassCarParking() {
+        CustomParking customParking = new CustomParking(2, 5);
+        Car[] passArray = new Car[5];
+        Car car11 = new PassangerCar("X111XX");
+        customParking.add(car11);
+        passArray[0] = car11;
+        assertArrayEquals(passArray, customParking.getPassangerCarParking().getCarArray());
+    }
+
+    @Test
+    public void whenPassCarGoesToTruckParking() {
+        CustomParking customParking = new CustomParking(2, 5);
+        Car[] truckArray = new Car[2];
+        Car car11 = new PassangerCar("X111XX");
+        customParking.getTruckParking().add(car11);
+        assertArrayEquals(truckArray, customParking.getTruckParking().getCarArray());
+    }
+
+    @Test
+    public void whenTruckGoesToTruckParking() {
+        CustomParking customParking = new CustomParking(2, 5);
+        Car[] truckArray = new Car[2];
+        Car car1 = new Truck("Y111YY", 2);
+        customParking.add(car1);
+        truckArray[0] = car1;
+        assertArrayEquals(truckArray, customParking.getTruckParking().getCarArray());
+    }
+
+    @Test
+    public void whenTruckGoesToPassCarParking() {
+        CustomParking customParking = new CustomParking(2, 5);
+        Car[] passArray = new Car[5];
+        Car car1 = new Truck("Y111YY", 2);
+        customParking.getPassangerCarParking().add(car1);
+        passArray[0] = car1;
+        passArray[1] = car1;
+        assertArrayEquals(passArray, customParking.getPassangerCarParking().getCarArray());
+    }
+
+    @Test
+    public void whenPassCarGoesTwiceToPassCarParking() {
+        CustomParking customParking = new CustomParking(2, 5);
+        Car[] passArray = new Car[5];
+        Car car11 = new PassangerCar("X111XX");
+        customParking.add(car11);
+        customParking.add(car11);
+        passArray[0] = car11;
+        assertArrayEquals(passArray, customParking.getPassangerCarParking().getCarArray());
+    }
+
+    @Test
+    public void whenTruckGoesTwiceToPassCarParking() {
+        CustomParking customParking = new CustomParking(2, 5);
+        Car[] passArray = new Car[5];
+        Car car1 = new Truck("Y111YY", 2);
+        customParking.getPassangerCarParking().add(car1);
+        customParking.getPassangerCarParking().add(car1);
+        passArray[0] = car1;
+        passArray[1] = car1;
+        assertArrayEquals(passArray, customParking.getPassangerCarParking().getCarArray());
+    }
+
+    @Test
+    public void whenPassCarGoesToFilledPassCarParking() {
+        CustomParking customParking = new CustomParking(2, 2);
+        Car[] passArray = new Car[2];
+        Car car11 = new PassangerCar("X111XX");
+        Car car22 = new PassangerCar("X222XX");
+        Car car55 = new PassangerCar("X555XX");
+        customParking.add(car11);
+        customParking.add(car22);
+        customParking.add(car55);
+        passArray[0] = car11;
+        passArray[1] = car22;
+        assertArrayEquals(passArray, customParking.getPassangerCarParking().getCarArray());
+    }
+
+    @Test
+    public void whenTruckGoesTofilledTruckParking() {
+        CustomParking customParking = new CustomParking(2, 1);
+        Car[] truckArray = new Car[2];
+        Car[] truckInPassParkArray = new Car[1];
+        Car car1 = new Truck("Y111YY", 2);
+        Car car2 = new Truck("Y222YY", 2);
+        Car car3 = new Truck("Y333YY", 2);
+        customParking.add(car1);
+        customParking.add(car2);
+        customParking.add(car2);
+        truckArray[0] = car1;
+        truckArray[1] = car2;
+        assertArrayEquals(truckArray, customParking.getTruckParking().getCarArray());
+        assertArrayEquals(truckInPassParkArray, customParking.getPassangerCarParking().getCarArray());
+    }
+
+
+    @Test
     public void whenTruckParkingIsFilledAndTruckGoesToPassParkingWhichIsPartlyFilledAndTheAnotherOnePassCarFilledPassParking() {
         CustomParking customParking = new CustomParking(2, 5);
         Car[] truckArray = new Car[2];
